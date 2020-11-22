b93ebbc4c5572e10588c397fa7d6d9e2306b77a5
Taewoo Eom <teom@c8r7s3.42seoul.kr>
2020-11-23 00:08:07 +0900

    shell_assignmets

---
+++ sh_01/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | cut -d : -f 1 | grep -v "#" | awk '{if(NR%2==0)print}' | rev | sort -r | awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2" 'NR >= FT_LINE1 && NR<=FT_LINE2 { print $0 }' | awk '{ printf $0", " }' | sed "s/, $/./g" | tr -d "\n"
