a61ad5c618e6a6937bd5f72ee406f682aa1fbdcf
teom142 <xodn9245@naver.com>
2020-11-29 00:29:24 +0900

    c02_ex10

--- c_02/ex10/ft_strlcpy.c
+++ c_02/ex10/ft_strlcpy.c
@@ -15,7 +15,7 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int		index;
 
 	index = 0;
-	while (src[index] && index < size - 1)
+	while (src[index] && index + 1 < size)
 	{
 		dest[index] = src[index];
 		index++;
