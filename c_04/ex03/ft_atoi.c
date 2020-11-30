bde011e82d7c1c75cfed844d5de391e67a44360e
Taewoo Eom <teom@c10r7s4.42seoul.kr>
2020-11-30 15:52:44 +0900

    c04_ex03

--- c_04/ex03/ft_atoi.c
+++ c_04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: teom <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 00:32:10 by teom              #+#    #+#             */
-/*   Updated: 2020/11/30 05:18:50 by teom             ###   ########.fr       */
+/*   Updated: 2020/11/30 15:40:33 by teom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,31 @@ int		ft_atoi(char *str)
 {
 	int minus_num;
 	int val_atoi;
+	int val_end;
 
+	val_end = 1;
 	val_atoi = 0;
-	minus_num = 0;
+	minus_num = 1;
 	while (*str)
 	{
-		if (*str == 45)
-			minus_num++;
-		if (it_is_num(*str))
+		if (it_is_num(*str) && val_end)
 		{
 			while (it_is_num(*str))
 			{
 				val_atoi = (val_atoi * 10) + (*str - '0');
 				str++;
 			}
-			if (minus_num % 2)
-				return (-1 * val_atoi);
-			else
-				return (val_atoi);
+			val_end = 0;
 		}
+		if(*str == '-')
+			minus_num *= -1;
 		str++;
 	}
-	return (0);
+	return (val_atoi * minus_num);
+}
+
+#include <stdio.h>
+int		main(int argc, char *argv[])
+{
+	printf("%d\n",ft_atoi(argv[1]));
 }
