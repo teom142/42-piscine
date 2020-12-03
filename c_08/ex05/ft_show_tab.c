344de360cc0cc59cc90f209fe31684505b333c79
Taewoo Eom <teom@c8r7s3.42seoul.kr>
2020-12-04 00:34:23 +0900

    c08_ex05

---
+++ c_08/ex05/ft_show_tab.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teom </var/mail/teom>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/04 00:18:45 by teom              #+#    #+#             */
+/*   Updated: 2020/12/04 00:31:35 by teom             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_size(int size)
+{
+	if (size >= 10)
+		print_size(size / 10);
+	ft_putchar(size % 10 + '0');
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		write(1, par[i].str, par[i].size);
+		write(1, "\n", 1);
+		print_size(par[i].size);
+		write(1, "\n", 1);
+		write(1, par[i].copy, par[i].size);
+		write(1, "\n", 1);
+		i++;
+	}
+}
