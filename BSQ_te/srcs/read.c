b478c02d5bb6fc4c5d25e5f643d92f7920a08749
teom142 <xodn9245@naver.com>
2020-12-09 03:12:12 +0900

    hm

--- BSQ_te/srcs/read.c
+++ BSQ_te/srcs/read.c
@@ -6,7 +6,6 @@ extern char	g_empty;
 extern char	g_obstacle;
 extern char	g_fill;
 extern unsigned int g_col;
-char	*g_temp;
 
 void	get_symbols()
 {
@@ -19,24 +18,50 @@ void	get_symbols()
 	g_symbol[sym_len - 3] = 0;
 }
 
-void    read_map_sub(char *str, char c, unsigned int size)
+char	*expand_str(char *str, unsigned int size)
 {
-	ft_strcpy(g_temp, str);
+	char *ret;
+
+	if (!(ret = (char*)malloc(size + 2)))
+		exit(1);
+	ft_strcpy(ret, str);
 	free(str);
-	*str = 0;
-	str = (char*)malloc(size + 2);
-	ft_strcpy(str, g_temp);
-	ft_charcat(str, c);
-	free(g_temp);
-	*g_temp = 0;
+	return (ret);
 }
 #include <stdio.h>
+void	read_map_sub(int fd, char **map, int rd_suc, char c)
+{
+	unsigned int	i;
+	unsigned int	size;
+
+	if(!(map = (char**)malloc(sizeof(char*) * g_col)))
+		exit (1);
+	i = 0;
+	while (i < g_col)
+	{
+		size = 1;
+		if (!(map[i] = (char*)malloc(size + 1)))
+			exit(1);
+		printf("%c",c);
+		rd_suc = read(fd, &c, 1);
+		while (rd_suc && c != '\n')
+		{
+			map[i][size - 1] = c;
+			map[i][size] = 0;
+			map[i] = expand_str(map[i], size);
+			rd_suc = read(fd, &c, 1);
+			size++;
+		}
+		i++;
+	}
+}
+		
 void	read_map(int fd, char **map)
 {
 	int	rd_suc;
 	unsigned int size;
 	char	c;
-	unsigned int i;
+//	unsigned int i;
 
 	size = 1;
 	if (fd == -1)
@@ -48,34 +73,35 @@ void	read_map(int fd, char **map)
 				exit(1);
 		while (rd_suc && c != '\n')
 		{
-			g_temp = (char*)malloc(size + 1);
-			read_map_sub(g_symbol, c, size);
+			g_symbol[size - 1] = c;
+			g_symbol[size] = 0;
+			g_symbol = expand_str(g_symbol, size);
 			rd_suc = read(fd, &c, 1);
 			size++;
 		}
 		get_symbols();
 		g_col = count_col(g_symbol);
-		//printf("%c %c %c %u\n",g_empty, g_obstacle, g_fill, g_col);
-		if(!(map = (char**)malloc(sizeof(char*) * g_col)))
-			exit(1);
+		//if(!(map = (char**)malloc(sizeof(char*) * g_col)))
+		//	exit(1);
+		read_map_sub(fd, map, rd_suc, c);
+		/*
 		i = 0;
-		printf("plz");
 		while (i < g_col)
 		{
 			size = 1;
 			if(!(map[i] = (char*)malloc(size + 1)))
 				exit(1);
-			map[i][0] = 0;
 			rd_suc = read(fd, &c, 1);
-			while (c != '\n')
+			while (c != '\n' && rd_suc)
 			{
-				g_temp = (char*)malloc(size + 1);
-				read_map_sub(map[i], c, size);
+				map[i][size - 1] = c;
+				map[i][size] = 0;
+				map[i] = expand_str(map[i], size);
 				rd_suc = read(fd, &c, 1);
 				size++;
 			}
 			i++;
-		}
+		}*/
 		disp_map(map, g_col);
 	}
 	close(fd);
